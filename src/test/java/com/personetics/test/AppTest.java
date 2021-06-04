77bad0a14485f75e781a2d99a77699eda9f88c4e
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-06-04 16:42:02 -0300

    Implementing some the tests of the text of challenge

--- src/test/java/com/personetics/test/AppTest.java
+++ src/test/java/com/personetics/test/AppTest.java
@@ -4,15 +4,53 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    private ChainValidator cValidator = new ChainValidator();
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testChainOne() {
+        List<Integer> numbers = Arrays.asList(35, 5, 65, 6, 24, 4);
+
+        List<Node> nodes = new ArrayList<Node>();
+
+        numbers.forEach((n) -> {
+            Node node = new Node(n);
+            nodes.add(node);
+        });
+
+        assertEquals(false, cValidator.validate(nodes));
+    }
+
+    @Test
+    void testChainTwo() {
+        List<Integer> numbers = Arrays.asList(36, 6, 24, 4, 47, 7, 2, 3, 27);
+        List<Node> nodes = new ArrayList<Node>();
+
+        numbers.forEach((n) -> {
+            Node node = new Node(n);
+            nodes.add(node);
+        });
+
+        assertEquals(true, cValidator.validate(nodes));
+    }
+
+    @Test
+    void testChainThree() {
+        List<String> strings = Arrays.asList("p", "aba", "a", "b", "perso", "o", "r", "e", "s");
+        List<Node> nodes = new ArrayList<Node>();
+
+        strings.forEach((n) -> {
+            Node node = new Node(n);
+            nodes.add(node);
+        });
+
+        assertEquals(true, cValidator.validate(nodes));
     }
 }
