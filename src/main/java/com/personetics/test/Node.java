611a72df560a3c4aa1fabbc366286daeed5af1e1
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-06-04 16:31:57 -0300

    Implementing my Node class

---
+++ src/main/java/com/personetics/test/Node.java
@@ -0,0 +1,33 @@
+package com.personetics.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Node {
+    private Object value;
+
+    public Node(Object input) {
+        this.setValue(input);
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public ArrayList<String> getDependencies() {
+        ArrayList<String> itemsFromValue = new ArrayList<String>(Arrays.asList(this.getValue().toString().split("")));
+
+        // creating a set to remove items that are repeated
+        Set<String> set = new HashSet<>(itemsFromValue);
+        itemsFromValue.clear();
+        itemsFromValue.addAll(set);
+
+        return itemsFromValue;
+    }
+}
